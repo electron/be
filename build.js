3921fcc2315681512186721013b8ef2d974d7e00
Cheng Zhao <zcbenz@gmail.com>
2018-10-24 10:54:24 +0900

    Automatically run sccache server

--- build.js
+++ build.js
@@ -2,6 +2,20 @@
 
 const {argv, execSync} = require('./lib/common')
 
+const path = require('path')
+
+// Make sure sccache server is up.
+const sccache = path.resolve('src', 'electron', 'external_binaries', 'sccache')
+try {
+  execSync(`${sccache}  --start-server`, {stdio: 'pipe'})
+} catch (e) {
+  // Print error if not the server already running error.
+  if (!e.stderr.toString().startsWith('error: Server startup error')) {
+    process.stderr.write(`sccache server failed to start:\n${e.stderr}`)
+    process.exit(1)
+  }
+}
+
 // The configuration to build.
 let config = 'Default'
 if (argv.length === 1)
