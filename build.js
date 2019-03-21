538da7cf0adb419a10e20adec7534cc9cdb5782b
Cheng Zhao <zcbenz@gmail.com>
2019-03-21 13:33:05 +0900

    Wait for sccache server to start

--- build.js
+++ build.js
@@ -6,10 +6,16 @@ const path = require('path')
 
 // Make sure sccache server is up.
 const sccache = path.resolve('src', 'electron', 'external_binaries', 'sccache')
-try {
-  execSync(`${sccache}  --start-server`, {stdio: 'pipe'})
-} catch (e) {
-  // Ignore error of sccache.
+while (true) {
+  try {
+    execSync(`${sccache} -s`, {stdio: 'pipe'})
+    break
+  } catch (e) {
+    if (!e.message.includes('error: Connection to server timed out') &&
+        !e.message.includes('error: failed to get stats from server')) {
+      throw e
+    }
+  }
 }
 
 // The configuration to build.
